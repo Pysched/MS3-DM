8c225934ef30ea13c1e0ca299b9eb9021e8ca2a4
Pysched <murphyjdarren@gmail.com>
2020-06-01 12:23:58 +0000

    created login.html and referenced it in app.py and nav.html

--- app.py
+++ app.py
@@ -35,6 +35,12 @@ def browse():
     return render_template("browse.html")
 
 
+# Login Page
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get("PORT", "5000")), debug=True)
