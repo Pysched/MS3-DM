d8e99f75b461fcef82e36267a946b0fe455c14f1
Pysched <murphyjdarren@gmail.com>
2020-06-01 12:28:33 +0000

    created add_item.html and register.html pages and references in nav

--- app.py
+++ app.py
@@ -41,6 +41,18 @@ def login():
     return render_template("login.html")
 
 
+# Register Page
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+
+# Add Item Page
+@app.route('/add_item')
+def add_item():
+    return render_template("add_item.html")
+
+
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get("PORT", "5000")), debug=True)
