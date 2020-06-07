a806f91cba0c877c8a3d26ca04bce7d9ec9f3a88
Pysched <murphyjdarren@gmail.com>
2020-06-07 00:50:18 +0000

    changed the way of going about for updating user password

--- app.py
+++ app.py
@@ -275,4 +275,4 @@ def update_password(username):
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
-    app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get("PORT", "5000")), debug=False)
+    app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get("PORT", "5000")), debug=True)
