73520c342409b8825189871b7f970e44233138d7
Pysched <murphyjdarren@gmail.com>
2020-06-13 10:53:53 +0000

    updated styling

--- app.py
+++ app.py
@@ -417,7 +417,6 @@ def remove_listing(listings_id):
 
 
 if __name__ == '__main__':
-    
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
